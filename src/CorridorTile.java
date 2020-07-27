9ef7a5bad928c6da6bbc89ac8d13eab9c034b5f3
ducky-V <izaakkarasawa@gmail.com>
2020-07-27 17:03:10 +1200

    CorridorTile.java: + Constructor for CorridorTile + getWalls() + comments for class and the above methods

--- src/CorridorTile.java
+++ src/CorridorTile.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+/**
+ * Class for the Tiles that are in the corridor(Non-room tiles).
+ */
 public class CorridorTile implements Tile {
+
+    /**
+     * Constructor for CorridorTile
+     */
+    public CorridorTile() {
+    }
+
+    /**
+     * Getter for the walls each tile has.
+     * @return an ArrayList of Directions, the directions represent where walls are.
+     */
+    @Override
+    public ArrayList<Direction> getWalls() {
+        return this.walls;
+    }
 }
