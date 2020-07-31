a3d027e44a54b79639e085f343762bbf9cad8e48
Tim <animae.de.tenebris@hotmail.co.uk>
2020-07-31 17:53:41 +1200

    Board tiles constructed

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,3 @@
-import Cards.RoomCard;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -40,8 +37,11 @@ public class Board {
     private HashMap<String,Room> rooms;
     private Tile[][] board;
 
+    /**
+     * Initialises the playing board
+     */
     public Board() {
-        board = new Tile[25][24];
+        board = new Tile[26][25]; //[0][0] Not used to allow for easier indexing - A1 == [1][1]
         rooms = new HashMap<>();
         //Load rooms//
         loadKitchen();
@@ -54,10 +54,14 @@ public class Board {
         loadHall();
         loadStudy();
         //Add corridors and walls
+        loadCellar();
         loadCorridor();
         loadWalls();
     }
 
+    /**
+     * Loads the Kitchen tiles, and adds Kitchen to the rooms list
+     */
     private void loadKitchen() {
         String room = "Kitchen";
         rooms.put(room, new Room(room));
@@ -79,8 +83,13 @@ public class Board {
             board[((int)'E'-65)][i] = new RoomTile(rooms.get(room));
         for (int i: F)
             board[((int)'F'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: G)
+            board[((int)'G'-65)][i] = new RoomTile(rooms.get(room));
     }
 
+    /**
+     * Loads the Ballroom tiles, and adds Ballroom to the rooms list
+     */
     private void loadBallroom() {
         String room = "Ballroom";
         rooms.put(room, new Room(room));
@@ -109,15 +118,38 @@ public class Board {
             board[((int)'H'-65)][i] = new RoomTile(rooms.get(room));
     }
 
+    /**
+     * Loads the Conservatory tiles, and adds Conservatory to the rooms list
+     */
     private void loadConservatory() {
+        String room = "Conservatory";
+        rooms.put(room, new Room(room));
+
         int[] B = {19,20,21,22,23,24};
         int[] C = {19,20,21,22,23,24};
         int[] D = {19,20,21,22,23,24};
         int[] E = {19,20,21,22,23,24};
         int[] F = {20,21,22,23};
+
+        for (int i: B)
+            board[((int)'B'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: C)
+            board[((int)'C'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: D)
+            board[((int)'D'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: E)
+            board[((int)'E'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: F)
+            board[((int)'F'-65)][i] = new RoomTile(rooms.get(room));
     }
 
+    /**
+     * Loads the Dining Room tiles, and adds Dining Room to the rooms list
+     */
     private void loadDiningRoom() {
+        String room = "Dining Room";
+        rooms.put(room, new Room(room));
+
         int[] J = {1,2,3,4,5};
         int[] K = {1,2,3,4,5,6,7,8};
         int[] L = {1,2,3,4,5,6,7,8};
@@ -125,34 +157,108 @@ public class Board {
         int[] N = {1,2,3,4,5,6,7,8};
         int[] O = {1,2,3,4,5,6,7,8};
         int[] P = {1,2,3,4,5,6,7,8};
+
+        for (int i: J)
+            board[((int)'J'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: K)
+            board[((int)'K'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: L)
+            board[((int)'L'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: M)
+            board[((int)'M'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: N)
+            board[((int)'N'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: O)
+            board[((int)'O'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: P)
+            board[((int)'P'-65)][i] = new RoomTile(rooms.get(room));
     }
 
+    /**
+     * Loads the Billiard Room tiles, and adds Billiard Room to the rooms list
+     */
     private void loadBilliardRoom() {
+        String room = "Billiard Room";
+        rooms.put(room, new Room(room));
+
         int[] I = {19,20,21,22,23,24};
         int[] J = {19,20,21,22,23,24};
         int[] K = {19,20,21,22,23,24};
         int[] L = {19,20,21,22,23,24};
         int[] M = {19,20,21,22,23,24};
+
+        for (int i: I)
+            board[((int)'I'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: J)
+            board[((int)'J'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: K)
+            board[((int)'K'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: L)
+            board[((int)'L'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: M)
+            board[((int)'M'-65)][i] = new RoomTile(rooms.get(room));
     }
 
+    /**
+     * Loads the Library tiles, and adds Library to the rooms list
+     */
     private void loadLibrary() {
+        String room = "Library";
+        rooms.put(room, new Room(room));
+
         int[] O = {19,20,21,22,23};
         int[] P = {18,19,20,21,22,23,24};
         int[] Q = {18,19,20,21,22,23,24};
         int[] R = {18,19,20,21,22,23,24};
         int[] S = {19,20,21,22,23};
+
+        for (int i: O)
+            board[((int)'O'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: P)
+            board[((int)'P'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: Q)
+            board[((int)'Q'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: R)
+            board[((int)'R'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: S)
+            board[((int)'S'-65)][i] = new RoomTile(rooms.get(room));
     }
 
+    /**
+     * Loads the Lounge tiles, and adds Lounge to the rooms list
+     */
     private void loadLounge() {
+        String room = "Lounge";
+        rooms.put(room, new Room(room));
+
         int[] T = {1,2,3,4,5,6,7};
         int[] U = {1,2,3,4,5,6,7};
         int[] V = {1,2,3,4,5,6,7};
         int[] W = {1,2,3,4,5,6,7};
         int[] X = {1,2,3,4,5,6,7};
         int[] Y = {1,2,3,4,5,6};
+
+        for (int i: T)
+            board[((int)'T'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: U)
+            board[((int)'U'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: V)
+            board[((int)'V'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: W)
+            board[((int)'W'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: X)
+            board[((int)'X'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: Y)
+            board[((int)'Y'-65)][i] = new RoomTile(rooms.get(room));
     }
 
+    /**
+     * Loads the Hall tiles, and adds Hall to the rooms list
+     */
     private void loadHall() {
+        String room = "Hall";
+        rooms.put(room, new Room(room));
+
         int[] S = {10,11,12,13,14,15};
         int[] T = {10,11,12,13,14,15};
         int[] U = {10,11,12,13,14,15};
@@ -160,16 +266,74 @@ public class Board {
         int[] W = {10,11,12,13,14,15};
         int[] X = {10,11,12,13,14,15};
         int[] Y = {11,12,13,14};
+
+        for (int i: S)
+            board[((int)'S'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: T)
+            board[((int)'T'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: U)
+            board[((int)'U'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: V)
+            board[((int)'V'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: W)
+            board[((int)'W'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: X)
+            board[((int)'X'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: Y)
+            board[((int)'Y'-65)][i] = new RoomTile(rooms.get(room));
     }
 
+    /**
+     * Loads the Study tiles, and adds Study to the rooms list
+     */
     private void loadStudy() {
+        String room = "Study";
+        rooms.put(room, new Room(room));
+
         int[] V = {18,19,20,21,22,23,24};
         int[] W = {18,19,20,21,22,23,24};
         int[] X = {18,19,20,21,22,23,24};
         int[] Y = {19,20,21,22,23,24};
+
+        for (int i: V)
+            board[((int)'V'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: W)
+            board[((int)'W'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: X)
+            board[((int)'X'-65)][i] = new RoomTile(rooms.get(room));
+        for (int i: Y)
+            board[((int)'Y'-65)][i] = new RoomTile(rooms.get(room));
     }
 
-    private void loadCorridor() {}
+    /**
+     * Loads the Cellar tiles, but does not add to rooms list
+     */
+    private void loadCellar() {
+        Room cellar = new Room("Cellar");
+        for(int y = (int)'K'-65; y <= (int)'Q'-65; y++)
+            for(int x = 11; x <= 15; x++)
+                board[y][x] = new RoomTile(cellar);
+    }
+
+    /**
+     * Fills in board with Corridor tiles
+     */
+    private void loadCorridor() {
+        //[B -> X][[2 -> 23]
+        for(int y = 2; y < board.length; y++)
+            for(int x = 2; x < board[x].length; x++)
+                if(board[y][x] == null)
+                    board[y][x] = new CorridorTile();
+        //Outlier corridor tiles (mostly used for starting points)
+        board[(int)'A'-65][10]  = new CorridorTile();
+        board[(int)'A'-65][15]  = new CorridorTile();
+        board[(int)'G'-65][24]  = new CorridorTile();
+        board[(int)'H'-65][1]   = new CorridorTile();
+        board[(int)'R'-65][1]   = new CorridorTile();
+        board[(int)'T'-65][24]  = new CorridorTile();
+        board[(int)'y'-65][8]   = new CorridorTile();
+        board[(int)'Y'-65][17]  = new CorridorTile();
+    }
 
     private void loadWalls() {
         //North Walls//
--- src/CorridorTile.java
+++ src/CorridorTile.java
@@ -3,23 +3,8 @@ import java.util.ArrayList;
  * Class for the Tiles that are in the corridor(Non-room tiles).
  */
 public class CorridorTile implements Tile {
-
-    private final ArrayList<Direction> walls;
-
     /**
      * Constructor for CorridorTile
-     * @param walls
      */
-    public CorridorTile(ArrayList<Direction> walls) {
-        this.walls = walls;
-    }
-
-    /**
-     * Getter for the walls each tile has.
-     * @return an ArrayList of Directions, the directions represent where walls are.
-     */
-    @Override
-    public ArrayList<Direction> getWalls() {
-        return this.walls;
-    }
+    public CorridorTile() {}
 }
